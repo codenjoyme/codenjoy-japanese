ea8a92803661f8fb35922967b4edd97c0f8b559f
Oleksandr_Baglai <apofig@gmail.com>
2020-12-02 06:38:38 +0200

    [japanese][delphi] Подготовил тестовый фреймворк к новым сценариям

--- src/test/java/com/codenjoy/dojo/japanese/model/portable/SolverTest.java
+++ src/test/java/com/codenjoy/dojo/japanese/model/portable/SolverTest.java
@@ -9,11 +9,8 @@ public class SolverTest {
 
     @Test
     public void test() {
-        Solver solver = new Solver();
-        solver.tryAssumption = true;
 
-        solver.load(new LevelImpl(
-                "-**---**-\n" +
+        assertS("-**---**-\n" +
                 "****-****\n" +
                 "*********\n" +
                 "*********\n" +
@@ -21,11 +18,8 @@ public class SolverTest {
                 "-*******-\n" +
                 "--*****--\n" +
                 "---***---\n" +
-                "----*----"));
+                "----*----",
 
-        solver.clear(false);
-
-        assertEquals(
                 "           \n" +
                 "  467777764\n" +
                 "22---------\n" +
@@ -36,11 +30,8 @@ public class SolverTest {
                 " 7---------\n" +
                 " 5---------\n" +
                 " 3---------\n" +
-                " 1---------\n", solver.printAll());
+                " 1---------\n",
 
-        solver.solve();
-
-        assertEquals(
                 "           \n" +
                 "  467777764\n" +
                 "22---------\n" +
@@ -51,7 +42,22 @@ public class SolverTest {
                 " 7--*****--\n" +
                 " 5----*----\n" +
                 " 3---------\n" +
-                " 1---------\n", solver.printAll());
+                " 1---------\n");
+    }
+
+    private void assertS(String map, String expected, String expected1) {
+        // given
+        Solver solver = new Solver();
+        solver.tryAssumption = true;
+        solver.load(new LevelImpl(map));
+        solver.clear(false);
+        assertEquals(expected, solver.printAll());
+
+        // when
+        solver.solve();
+
+        // then
+        assertEquals(expected1, solver.printAll());
     }
 
 }
