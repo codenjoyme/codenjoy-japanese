823d9c8fa6ab0f17770f073bff58a11b8f971bdc
Oleksandr_Baglai <apofig@gmail.com>
2020-12-05 03:10:25 +0200

    [japanese][delphi] Решено использовать Approvals подход

--- src/test/java/com/codenjoy/dojo/japanese/model/portable/LineSolverTest.java
+++ src/test/java/com/codenjoy/dojo/japanese/model/portable/LineSolverTest.java
@@ -1,12 +1,11 @@
 package com.codenjoy.dojo.japanese.model.portable;
 
+import org.approvaltests.legacycode.LegacyApprovals;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 
 public class LineSolverTest {
 
@@ -14,58 +13,156 @@ public class LineSolverTest {
 
     @Test
     public void test1() {
-        assertCombinations("{3,1}UUUUU->[B-100%, B-100%, B-100%, W-0%, B-100%]");
+        String[] dots = new String[]{
+                "U",
+                "UU",
+                "UUUU",
+                "UUUUU",
+                "UUUUUU",
+                "UUUUUUU",
+                "UUUUUUUU",
+                "UUUUUUUUU",
+                "UUUUUUUUUU",
+                "UUUUUUUUUUU",
+                "UUUUUUUUUUUU",
+                "UUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUUUUUUUUU",
+                "UUUUUUUUUUUUUUUUUUUUUUUUUUUU",
+                "B",
+                "UB",
+                "UBUU",
+                "UUBUU",
+                "UUBUBU",
+                "UBUBUBU",
+                "UUBUUBUU",
+                "UUBUUBUBU",
+                "UUBUUBBBUU",
+                "UUBUBUUBUUU",
+                "UUBBBUUBUUUU",
+                "BUUBUUUUUUUUU",
+                "BUUUUBUUUUUUUU",
+                "UUUUUBUUUUUUUUU",
+                "UUUUUUUUBUUUUUUU",
+                "UUUUUUUUUUUUUUUBB",
+                "UUUBUUUUUBUUUUBUUU",
+                "UUBUUUBUUUUUBUUUBUB",
+                "UUUUUUUUUUUUBUUUUUUU",
+                "UUUUUBUUBUUUUUUUUUUUU",
+                "UUUUUBUUBUUUUUUUBUUUUU",
+                "UUUUUBUUBUUUUBUUBUUUUUU",
+                "UUUUUBUUBUUUUBUUUUBBBUUU",
+                "UUUUUBUUBUUUUUUUBUBUUUUUU",
+                "UUUUUBUUBUUUUUUUBBUBUUUUUU",
+                "UUUUUBUUBUUUUUUUBBUBUUUUBUU",
+                "W",
+                "UW",
+                "UWUU",
+                "UUWUU",
+                "UUWUWU",
+                "UWUWUWU",
+                "UUWUUWUU",
+                "UUWUUWUWU",
+                "UUWUUWWWUU",
+                "UUWUWUUWUUU",
+                "UUWWWUUWUUUU",
+                "WUUWUUUUUUUUU",
+                "WUUUUWUUUUUUUU",
+                "UUUUUWUUUUUUUUU",
+                "UUUUUUUUWUUUUUUU",
+                "UUUUUUUUUUUUUUUWW",
+                "UUUWUUUUUWUUUUWUUU",
+                "UUWUUUWUUUUUWUUUWUW",
+                "UUUUUUUUUUUUWUUUUUUU",
+                "UUUUUWUUWUUUUUUUUUUUU",
+                "UUUUUWUUWUUUWUUUUWWUUU",
+                "UUUUUWUUWUUUWUUUUWWUUUU",
+                "UUUUUWUUWUUUWUUUUWWUUUUU",
+                "UUUUUWUUWUUUWUUWUWWUUUUUU",
+                "UUUUUWUUWWUUWUUWUWWUWWWWUU",
+                "UUUUUWUUWUUUWUUWUWWUUUWWUU",
+                "W",
+                "BW",
+                "UWUU",
+                "UBWUU",
+                "UUWBWU",
+                "UWUWUWU",
+                "UUWBUWUU",
+                "UUWBUWUWU",
+                "UUWUBWWWUU",
+                "UUWUWBUWUUU",
+                "UUWWWUUWUUUU",
+                "WUUWUBUUUUUUU",
+                "WUUBUWUUUUUUUU",
+                "UUUBUWUBUUWWWUU",
+                "UUUUUUUUWBWBWUUU",
+                "UUUUUBUUUBUUUUUWW",
+                "UUUWUBUUUWUUUUWUUU",
+                "UUWUUUWWWWBUWUUUWUW",
+                "UUUUUWUWUUBUWUUUUUUU",
+                "UUUBWWUUWUUUUUUUUUUUU",
+                "UUUBWWUUWUUUUBUUUUUUUU",
+                "UUUBWWUUWUUUUUUBUUUBUUU",
+                "UUUBWWUUWUUUUBUUUUUUBUUU",
+                "UUUBWWUUWUUUUBUUUUUUBUUUB",
+                "UUUBWWUUWUUUUBUUUUUUBUUUBB",
+                "UUUBWWUUWUUUUBUUUUUUBUUUBBB",
+        };
+        String[] numbers = new String[]{
+                "{1}",
+                "{2}",
+                "{1,2}",
+                "{2,1}",
+                "{3}",
+                "{1,2,3}",
+                "{1,3,3}",
+                "{1,3,3,9}",
+                "{1,5,3}",
+                "{1,5,3,1}",
+                "{1,5,3,2,2}",
+                "{5,7}",
+                "{9}",
+                "{9,1,1,1,1}",
+                "{1,1,1,1,9}",
+                "{2,2,2}",
+                "{3,2,1}",
+                "{3,4,2,1}",
+                "{1,1,1,1}",
+                "{1,3,2,1,1}",
+                "{1,3,1,1}",
+                "{1,1,1}",
+                "{1,1,1,1,1,1}",
+                "{1,1}",
+                "{1,4}",
+                "{4}",
+                "{2,3}",
+        };
+        LegacyApprovals.LockDown(this, "getCombinations", numbers, dots);
     }
 
-    @Test
-    public void test2() {
-        assertCombinations("{2}UU->[B-100%, B-100%]");
-    }
-
-    @Test
-    public void test3() {
-        assertCombinations("{1}UUU->[U-33%, U-33%, U-33%]");
-    }
-
-    @Test
-    public void test4() {
-        assertCombinations("{2}UUU->[U-50%, B-100%, U-50%]");
-    }
-
-    @Test
-    public void test5() {
-        assertCombinations("{2}UUUU->[U-33%, U-67%, U-67%, U-33%]");
-    }
-
-    @Test
-    public void test6() {
-        assertCombinations("{3}UUUU->[U-50%, B-100%, B-100%, U-50%]");
-    }
-
-    @Test
-    public void test7() {
-        assertCombinations("{3,1}UUUUUU->[U-67%, B-100%, B-100%, U-33%, U-33%, U-67%]");
-    }
-
-    private void assertCombinations(String data) {
-        String[] split = data.split("->");
-        String left = split[0];
-        String expected = split[1];
-
-        String[] inputs = left.split("}");
-        String numbers = inputs[0].replaceAll("\\{", "");
-        String dots = inputs[1];
-
-        assertCombinations(expected, dots, numbers);
-    }
-
-    private void assertCombinations(String expected, String dots, String inputNumbers) {
-        int[] numbers = Arrays.stream(("0," + inputNumbers).split(","))
+    public String getCombinations(String inputNumbers, String dots) {
+        int[] numbers = Arrays.stream(("0," + inputNumbers.replaceAll("[\\{\\}]", "")).split(","))
                 .mapToInt(i -> Integer.valueOf(i))
                 .toArray();
 
-        solver.calculate(numbers, parseDots(dots));
-        assertEquals(expected, formatResult());
+        try {
+            solver.calculate(numbers, parseDots(dots));
+            return formatResult();
+        } catch (Exception e) {
+            return e.getClass().getSimpleName();
+        }
     }
 
     private Dot[] parseDots(String string) {
@@ -79,7 +176,7 @@ public class LineSolverTest {
     private String formatResult() {
         List<String> data = new LinkedList<>();
         for (int i = 1; i <= solver.length(); i++) {
-            data.add(String.format("%s-%s%%",
+            data.add(String.format("%s:%s%%",
                     solver.dots(i).ch(),
                     Math.round(solver.probability(i) * 100)));
         }
