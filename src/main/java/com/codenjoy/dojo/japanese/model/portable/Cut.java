d1cf6fd9a7498b13fc5dc18e07c9c9101d500381
Oleksandr_Baglai <apofig@gmail.com>
2020-12-05 14:34:30 +0200

    [japanese][delphi] Рефакторим Cut

--- src/main/java/com/codenjoy/dojo/japanese/model/portable/Cut.java
+++ src/main/java/com/codenjoy/dojo/japanese/model/portable/Cut.java
@@ -6,6 +6,11 @@ import static com.codenjoy.dojo.japanese.model.portable.Solver.EXACTLY_NOT_BLACK
 public class Cut {
 
     private LineSolver solver;
+    private Dot previous;
+    private int numbersIndex;
+    private int countDots;  // количество точек в ряду
+    private int index;
+    private boolean stop;
 
     public Cut(LineSolver solver) {
         this.solver = solver;
@@ -15,29 +20,23 @@ public class Cut {
     // возвращает true если есть над чем гонять комбинации и даем возможность перебрать их
     // но перебирать будем только на тех числах, которые остались после оптимизации и в диапазоне точек
     // from ... to
-    // TODO продолжить рефакторить
     public boolean process() {
         // идем по ряду в прямом порядке
-        Dot previous = Dot.WHITE;
-        int numbersIndex = 0;
-        int countDots = 0; // количество точек в ряду
-        int i = 1;
-        solver.range().from(i);
-        boolean stop = false;
+        init(true);
         do {
-            switch (solver.dots(i)) {
+            switch (solver.dots(index)) {
                 case UNSET: {
                     if (previous.isBlack()) {
                         // UNSET после BLACK - надо заканчивать ряд
                         if (countDots < solver.numbers(numbersIndex)) {
                             // незакончили numbersIndex'ный ряд
                             countDots++; // количество точек
-                            solver.probabilities().set(i, EXACTLY_BLACK);
+                            solver.probabilities().set(index, EXACTLY_BLACK);
                             previous = Dot.BLACK;
                         } else {
                             // закончили numbersIndex'ный ряд
                             countDots = 0; // новый ряд еще не начали
-                            solver.probabilities().set(i, EXACTLY_NOT_BLACK);
+                            solver.probabilities().set(index, EXACTLY_NOT_BLACK);
                             previous = Dot.WHITE;
 
                             solver.SHLNumbers(); // сдвигаем ряд (удаляем первый элемент)
@@ -47,9 +46,9 @@ public class Cut {
                         // UNSET после WHITE
                         if (solver.countNumbers() == 0) {
                             // ряд пустой, тут все WHITE
-                            solver.probabilities().set(i, EXACTLY_NOT_BLACK);
+                            solver.probabilities().set(index, EXACTLY_NOT_BLACK);
                         } else {
-                            solver.range().from(i);
+                            solver.range().from(index);
                             stop = true; // иначе выходим
                         }
                     }
@@ -61,7 +60,7 @@ public class Cut {
                         countDots = 0;
                         previous = Dot.BLACK;
                     }
-                    solver.probabilities().set(i, EXACTLY_BLACK);
+                    solver.probabilities().set(index, EXACTLY_BLACK);
                     countDots++;
                 }
                 break;
@@ -76,45 +75,39 @@ public class Cut {
                         solver.SHLNumbers(); // сдвигаем ряд (удаляем первый элемент)
                         numbersIndex--; // из за смещения
                     }
-                    solver.probabilities().set(i, EXACTLY_NOT_BLACK);
+                    solver.probabilities().set(index, EXACTLY_NOT_BLACK);
                 }
                 break;
             }
-            i++;
+            index++;
             // TODO подумать почему тут нельзя перебирать комбинации
-            if ((!stop && i > solver.length()) || solver.countNumbers() == 0) return false; // достигли конца
+            if ((!stop && index > solver.length()) || solver.countNumbers() == 0) return false; // достигли конца
         } while (!stop);
 
-        // идем по ряду в обратном порядке
-        previous = Dot.WHITE;
-        numbersIndex = solver.countNumbers() + 1;
-        countDots = 0;
-        i = solver.length();
-        solver.range().to(i);
-        stop = false;
+        init(false);
         do {
-            switch (solver.dots(i)) {
+            switch (solver.dots(index)) {
                 case UNSET: {
                     if (previous.isBlack()) {
                         // UNSET после BLACK - надо заканчивать ряд
                         if (countDots < solver.numbers(numbersIndex)) {
                             // незакончили numbersIndex'ный ряд
                             countDots++; // количество точек
-                            solver.probabilities().set(i, EXACTLY_BLACK);
+                            solver.probabilities().set(index, EXACTLY_BLACK);
                             previous = Dot.BLACK;
                         } else {
                             // закончили numbersIndex'ный ряд
                             countDots = 0; // новый ряд еще не начали
-                            solver.probabilities().set(i, EXACTLY_NOT_BLACK);
+                            solver.probabilities().set(index, EXACTLY_NOT_BLACK);
                             previous = Dot.WHITE;
                             solver.countNumbers(solver.countNumbers() - 1);
                         }
                     } else {
                         // WHITE после пустоты
                         if (solver.countNumbers() == 0) { // в этом ряде ничего больше делать нечего
-                            solver.probabilities().set(i, EXACTLY_NOT_BLACK); // кончаем его:) // TODO тут скорее UNKNOWN
+                            solver.probabilities().set(index, EXACTLY_NOT_BLACK); // кончаем его:) // TODO тут скорее UNKNOWN
                         } else {
-                            solver.range().to(i);
+                            solver.range().to(index);
                             stop = true; // иначе выходим
                         }
                     }
@@ -126,7 +119,7 @@ public class Cut {
                         countDots = 0;
                         previous = Dot.BLACK;
                     }
-                    solver.probabilities().set(i, EXACTLY_BLACK);
+                    solver.probabilities().set(index, EXACTLY_BLACK);
                     countDots++;
                 }
                 break;
@@ -139,18 +132,35 @@ public class Cut {
                         previous = Dot.WHITE;
                         solver.countNumbers(solver.countNumbers() - 1);
                     }
-                    solver.probabilities().set(i, EXACTLY_NOT_BLACK);
+                    solver.probabilities().set(index, EXACTLY_NOT_BLACK);
                 }
                 break;
             }
-            i--;
+            index--;
             // TODO подумать почему тут нельзя перебирать комбинации
-            if ((!stop && i < 1) || solver.countNumbers() == 0) return false; // достигли конца
+            if ((!stop && index < 1) || solver.countNumbers() == 0) return false; // достигли конца
         } while (!stop);
 
         solver.range().calcLength();
         // если остались ряды точек то надо прогнать генератор, чем сообщаем возвращая true
         return solver.countNumbers() != 0 && solver.range().exists();
+    }
+
+    private void init(boolean order) {
+        countDots = 0;
+        stop = false;
+        previous = Dot.WHITE;
+        if (order) {
+            // идем по ряду в прямом порядке
+            numbersIndex = 0;
+            index = 1;
+            solver.range().from(index);
+        } else {
+            // идем по ряду в обратном порядке
+            numbersIndex = solver.countNumbers() + 1;
+            index = solver.length();
+            solver.range().to(index);
+        }
     }
 
 }
