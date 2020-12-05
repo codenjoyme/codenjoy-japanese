d502983c310f6eb5f2c7075a77f7f1a8cb4e4af3
Oleksandr_Baglai <apofig@gmail.com>
2020-12-05 03:46:17 +0200

    [japanese][delphi] Выделили новую сущность Blocks - где вся логика перемещения блоков черных точек между белых в заданном диапазоне

---
+++ src/main/java/com/codenjoy/dojo/japanese/model/portable/Blocks.java
@@ -0,0 +1,173 @@
+package com.codenjoy.dojo.japanese.model.portable;
+
+public class Blocks {
+
+    public static class Info {
+        public int length;
+        public boolean isBlack;
+    }
+    
+    private int current;
+    private Info[] items;
+
+    public Blocks(int len) {
+        items = new Info[len + 1];
+        for (int i = 1; i < items.length; i++) {
+            items[i] = new Info();
+        }
+    }
+
+    public void packTightToTheLeft(int[] numbers, int countNumbers, int range) {
+        // упаковываем максимально все блоки слева впритык друг к другу
+        // возвращаем количество блоков, которые удалось упаковать
+        current = 1;
+        int j = 0;
+        for (int numIndex = 1; numIndex <= countNumbers; numIndex++) {
+            // работаем попарно черный как сказано в числах
+            items[current].isBlack = true;
+            items[current].length = numbers[numIndex];
+
+            // потом 1 пиксель белый
+            items[current + 1].isBlack = false;
+            items[current + 1].length = 1;
+
+            // отступаем в ряду размеры новосозданных блоков
+            j += items[current].length + items[current + 1].length;
+            // всегда чередуются черный-белый попарно
+            current += 2;
+        }
+        current--; // сбрасываем последний increase чтобы block ссылался на WHITE из последней пары BLACK-WHITE
+
+        if (j > range) {
+            // убираем последний WHITE блок, если сумарная длинна не вписывается в range
+            current--;
+        } else if (j < range) {
+            // последний WHITE блок мы делаем длинной так, чтобы он заполнил остаток рабочей зоны range (от from до to)
+            // по умолчанию он длинной 1
+            items[current].length = items[current].length + range - j;
+        }
+    }
+
+    public void saveCombinations(int from, int to, boolean[] combinations) {
+        int offset = from - 1;
+        for (int block = 1; block <= items.length - 1; block++) {
+            for (int len = 1; len <= items[block].length; len++) {
+//                if (offset + len > len || block > len) {
+//                    break; // TODO этого не должно происходить, но происходит
+//                }
+                combinations[offset + len] = items[block].isBlack;
+            }
+            offset += items[block].length;
+        }
+    }
+
+    public void loadCombination(int from, int to, boolean[] combinations) {
+        boolean color = combinations[from];
+        int blockLength = 1;
+        current = 1;
+        for (int offset = from + 1; offset <= to; offset++) {
+//            if (offset >= combinations.length) break; // TODO этого не должно происходить но происходит
+            if (combinations[offset] ^ color) {
+                items[current].length = blockLength;
+                items[current].isBlack = color;
+                current++; ;
+                blockLength = 0;
+            }
+            blockLength++;
+            color = combinations[offset];
+        }
+        if (blockLength != 0) {
+            items[current].length = blockLength;
+            items[current].isBlack = color;
+        }
+    }
+
+    public boolean next() {
+        boolean b2 = false;
+        int a = current;
+        while (true) {
+            if (items[a].isBlack) {
+                a--;
+                if (a <= 0) {
+                    return false;
+                }
+                boolean b = true;
+                while (true) {
+                    int a2 = 2;
+                    if (!b) {
+                        continue;
+                    }
+
+                    if (a + 2 > current) {
+                        if (a + 2 != current + 1) break;
+                        if (!items[current].isBlack) break;
+
+                        b2 = false;
+                        if (items[a].length != 1 && current == 2) {
+                            b2 = true;
+                        }
+                        while (items[a].length == 1) {
+                            a -= 2;
+                            a2 += 2;
+                            if (a <= 0 || a2 == current) {
+                                b2 = true;
+                                break;
+                            }
+                        }
+
+                        if (b2) break;
+
+                        current++;
+                        items[current].isBlack = false;
+                        items[current].length = 0;
+                    }
+                    items[a + a2].length = items[a + a2].length + items[a].length - 2;
+                    items[a].length = 2;
+                    break;
+                }
+                if (b2) {
+                    return false;
+                }
+            } else {
+                if (current == 1) {
+                    return false;
+                }
+                if (a - 2 <= 0) {
+                    if (a < 1) break;
+                    current++;
+                    for (int i = current - 1; i >= 1; i--) {
+                        items[i + 1].length = items[i].length;
+                        items[i + 1].isBlack = items[i].isBlack;
+                    }
+                    items[1].length = 1;
+                    items[1].isBlack = false;
+                    if (current > 3) {
+                        items[3].length = 1;
+                    }
+                    if (current == 3) {
+                        items[3].length--;
+                    }
+                    break;
+                }
+                items[a - 2].length++;
+                items[a].length--;
+                if (items[a].length == 0) {
+                    if (a == current) {
+                        current--;
+                        break;
+                    } else {
+                        items[a - 2].length--;
+                        items[a].length++;
+                        a = a - 2;
+                    }
+                    continue;
+                } else {
+//                   current--;
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+}
--- src/main/java/com/codenjoy/dojo/japanese/model/portable/LineSolver.java
+++ src/main/java/com/codenjoy/dojo/japanese/model/portable/LineSolver.java
@@ -6,11 +6,6 @@ import static com.codenjoy.dojo.japanese.model.portable.Solver.*;
 
 public class LineSolver {
 
-    public static class Info {
-        public int length;
-        public boolean isBlack;
-    }
-
     private boolean[] combinations;
     private int combinationCount;
 
@@ -20,8 +15,8 @@ public class LineSolver {
 
     private int[] numbers;
     private int countNumbers;
-    private int currentBlock;
-    private Info[] blocks;
+
+    private Blocks blocks;
 
     private int from;
     private int to;
@@ -74,19 +69,17 @@ public class LineSolver {
         // TODO тут в некоторых тестах case_b15 случается переполнение, потому тут массив большой
         combinations = new boolean[len + 1 + 100];
         probability = new double[len + 1 + 100];
-        blocks = new Info[len + 1];
-        for (int i = 1; i < blocks.length; i++) {
-            blocks[i] = new Info();
-        }
+
+        blocks = new Blocks(len);
     }
 
     private boolean generateCombinations() {
-        currentBlock = packBlocksTightToTheLeft();
+        blocks.packTightToTheLeft(numbers, countNumbers, range);
 
         // пошли генерить комбинации
         combinationCount = 0;
         do {
-            getCombinationsFromNumbers(currentBlock);
+            blocks.saveCombinations(from, to, combinations);
             if (testCombination()) {
                 combinationCount++;
 
@@ -96,8 +89,8 @@ public class LineSolver {
                     }
                 }
             }
-            currentBlock = getNumbersFromCombination();
-        } while (manipulateNumbers());
+            blocks.loadCombination(from, to, combinations);
+        } while (blocks.next());
 
         for (var i = from; i <= to; i++) {
             if (combinationCount != 0) {
@@ -107,38 +100,6 @@ public class LineSolver {
             }
         }
         return combinationCount != 0;
-    }
-
-    // упаковываем максимально все блоки слева впритык друг к другу
-    // возвращаем количество блоков, которые удалось упаковать
-    private int packBlocksTightToTheLeft() {
-        int block = 1;
-        int j = 0;
-        for (int numIndex = 1; numIndex <= countNumbers; numIndex++) {
-            // работаем попарно черный как сказано в числах
-            blocks[block].isBlack = true;
-            blocks[block].length = numbers[numIndex];
-
-            // потом 1 пиксель белый
-            blocks[block + 1].isBlack = false;
-            blocks[block + 1].length = 1;
-
-            // отступаем в ряду размеры новосозданных блоков
-            j += blocks[block].length + blocks[block + 1].length;
-            // всегда чередуются черный-белый попарно
-            block += 2;
-        }
-        block--; // сбрасываем последний increase чтобы block ссылался на WHITE из последней пары BLACK-WHITE
-
-        if (j > range) {
-            // убираем последний WHITE блок, если сумарная длинна не вписывается в range
-            block--;
-        } else if (j < range) {
-            // последний WHITE блок мы делаем длинной так, чтобы он заполнил остаток рабочей зоны range (от from до to)
-            // по умолчанию он длинной 1
-            blocks[block].length = blocks[block].length + range - j;
-        }
-        return block;
     }
 
     // после того, как у нас есть массив вероятностей для всех возможных комбинаций
@@ -172,129 +133,6 @@ public class LineSolver {
                     break;
                 case ASSUMPTION:
                     break;
-            }
-        }
-        return true;
-    }
-
-    private void getCombinationsFromNumbers(int countBlocks) {
-        int offset = from - 1;
-        for (int block = 1; block <= countBlocks; block++) {
-            for (int len = 1; len <= blocks[block].length; len++) {
-//                if (offset + len > len || block > len) {
-//                    break; // TODO этого не должно происходить, но происходит
-//                }
-                combinations[offset + len] = blocks[block].isBlack;
-            }
-            offset += blocks[block].length;
-        }
-    }
-
-    private int getNumbersFromCombination() {
-        boolean color = combinations[from];
-        int blockLength = 1;
-        int block = 1;
-        for (int offset = from + 1; offset <= to; offset++) {
-//            if (offset >= combinations.length) break; // TODO этого не должно происходить но происходит
-            if (combinations[offset] ^ color) {
-                blocks[block].length = blockLength;
-                blocks[block].isBlack = color;
-                block++; ;
-                blockLength = 0;
-            }
-            blockLength++;
-            color = combinations[offset];
-        }
-        if (blockLength != 0) {
-            blocks[block].length = blockLength;
-            blocks[block].isBlack = color;
-        }
-        return block;
-    }
-
-    private boolean manipulateNumbers() {
-        boolean b2 = false;
-        int a = currentBlock;
-        while (true) {
-            if (blocks[a].isBlack) {
-                a--;
-                if (a <= 0) {
-                    return false;
-                }
-                boolean b = true;
-                while (true) {
-                    int a2 = 2;
-                    if (!b) {
-                        continue;
-                    }
-
-                    if (a + 2 > currentBlock) {
-                        if (a + 2 != currentBlock + 1) break;
-                        if (!blocks[currentBlock].isBlack) break;
-
-                        b2 = false;
-                        if (blocks[a].length != 1 && currentBlock == 2) {
-                            b2 = true;
-                        }
-                        while (blocks[a].length == 1) {
-                            a -= 2;
-                            a2 += 2;
-                            if (a <= 0 || a2 == currentBlock) {
-                                b2 = true;
-                                break;
-                            }
-                        }
-
-                        if (b2) break;
-
-                        currentBlock++;
-                        blocks[currentBlock].isBlack = false;
-                        blocks[currentBlock].length = 0;
-                    }
-                    blocks[a + a2].length = blocks[a + a2].length + blocks[a].length - 2;
-                    blocks[a].length = 2;
-                    break;
-                }
-                if (b2) {
-                    return false;
-                }
-            } else {
-                if (currentBlock == 1) {
-                    return false;
-                }
-                if (a - 2 <= 0) {
-                    if (a < 1) break;
-                    currentBlock++;
-                    for (int i = currentBlock - 1; i >= 1; i--) {
-                        blocks[i + 1].length = blocks[i].length;
-                        blocks[i + 1].isBlack = blocks[i].isBlack;
-                    }
-                    blocks[1].length = 1;
-                    blocks[1].isBlack = false;
-                    if (currentBlock > 3) {
-                        blocks[3].length = 1;
-                    }
-                    if (currentBlock == 3) {
-                        blocks[3].length--;
-                    }
-                    break;
-                }
-                blocks[a - 2].length++;
-                blocks[a].length--;
-                if (blocks[a].length == 0) {
-                    if (a == currentBlock) {
-                        currentBlock--;
-                        break;
-                    } else {
-                        blocks[a - 2].length--;
-                        blocks[a].length++;
-                        a = a - 2;
-                    }
-                    continue;
-                } else {
-//                   currentBlock--;
-                    break;
-                }
             }
         }
         return true;
