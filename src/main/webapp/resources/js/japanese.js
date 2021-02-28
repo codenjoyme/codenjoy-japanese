8b4eace14a778c50095ff50b2803dba55e5c12c4
Oleksandr_Baglai <apofig@gmail.com>
2021-03-01 00:13:08 +0200

    [japanese] Remove unused code.

--- src/main/webapp/resources/js/japanese.js
+++ src/main/webapp/resources/js/japanese.js
@@ -19,52 +19,5 @@
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-// ты можешь включать или выключать те или иные компоненты
-// например этот скрипт настроен на то, что будет только борда и
-// встроится она в layout указанный в ресурсах
-// если заремарить это все, тогда UI будет базовый
 
 var doNothing = true;
-
-/*
-game.enableDonate = false;
-game.enableJoystick = true;
-game.enableAlways = true;
-game.enablePlayerInfo = false;
-game.enableLeadersTable = false;
-game.enableHotkeys = true;
-game.enableAdvertisement = false;
-game.showBody = false;
-
-game.onBoardPageLoad = function() {
-    initLayout(game.gameName, 'board.html', game.contextPath,
-        function() {
-            $("#glasses").before($("#main_board"));
-            $("#main_board").remove();
-
-            $("#leaderboard").before($("#main_leaderboard"));
-            $("#main_leaderboard").remove();
-        },
-        ['js/lib1/script1.js',
-            'js/lib2/script1.js',
-            'js/lib2/script2.js',
-        ],
-        function() {
-            // setup UI
-
-            $(document.body).show();
-        });
-}
-
-game.onBoardAllPageLoad = function() {
-    initLayout(game.gameName, 'leaderboard.html', game.contextPath,
-        null,
-        [],
-        function() {
-            // setup UI
-
-            $(document.body).show();
-        });
-}
-
-*/
